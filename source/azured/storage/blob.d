bf0bb73a9cf255283082b36213ea9f3d316f245d
Adam Wilson <adamw@ellipticbit.com>
2016-05-27 01:36:32 -0700

    Stared working on Put/Get for Block Blobs.

--- source/azured/storage/blob.d
+++ source/azured/storage/blob.d
@@ -2,3 +2,59 @@ module azured.storage.blob;
 
 import azured.storage.client;
 import azured.storage.security;
+
+public enum AzureBlobType
+{
+	Block,
+	Append,
+	Page,
+}
+
+public final class AzureStorageBlobClient : AzureStorageClient
+{
+	this(string account, string sharedKey)
+	{
+		super(account, sharedKey);
+	}
+
+	this(string account, ubyte[] sharedKey)
+	{
+		super(account, sharedKey);
+	}
+}
+
+public void putBlob(AzureStorageBlobClient client, string container, string path, ubyte[] data)
+{
+	KeyVaultSecret ns;
+	requestHTTP(format("https://%s.blob.core.windows.net/%s/%s", client.Account, container, path),
+		(scope req) {
+			req.method = HTTPMethod.PUT;
+			req.httpVersion = HTTPVersion.HTTP_1_1;
+			req.contentType = "application/octet-stream"
+			req.bodyWriter.write(data)
+			client.writeRequiredHeaders(req, AzureBlobType.Block, container ~ "/" ~ path);
+		},
+		(scope res) {
+			if(res.statusCode != 201)
+				throw new Exception("Unable to upload blob.");
+		}
+	);
+	return ns;
+}
+
+public ubyte[] getBlob(AzureStorageBlobClient client, string container, string path)
+{
+	KeyVaultSecret ns;
+	requestHTTP(format("https://%s.blob.core.windows.net/%s/%s", client.Account, container, path),
+		(scope req) {
+			req.method = HTTPMethod.GET;
+			req.httpVersion = HTTPVersion.HTTP_1_1;
+			client.writeRequiredHeaders(req, AzureBlobType.Block, container ~ "/" ~ path);
+		},
+		(scope res) {
+			if(res.statusCode != 200)
+				throw new Exception("Unable to upload blob.");
+		}
+	);
+	return ns;
+}
--- source/azured/storage/security.d
+++ source/azured/storage/security.d
@@ -14,15 +14,16 @@ import vibe.textfilter.urlencode;
 import vibe.http.client;
 
 import azured.storage.client;
+import azured.storage.client;
 
-private string getGenericAuthorizationToken(AzureStorageClient conn, HTTPClientRequest req, string path, string comp, SysTime time)
+private string getAuthorizationToken(AzureStorageBlobClient conn, HTTPClientRequest req, AzureBlobType blobType, string path, string comp, SysTime time)
 {
 	string cmd5 =  req.headers.get("Content-MD5");
 	string ct = req.contentType;
 	string ctp = "; " ~ req.contentTypeParameters;
 	string crs = format("/%s/%s", conn.Account, path);
-	if(comp !is null || comp != "") crs ~= "?comp=" ~ comp;
-	string chd = format("x-ms-date:%s\nx-ms-version:%s\n", toRFC822DateTimeString(time), StorageApiVersion);
+	if(comp !is null && comp != "") crs ~= "?comp=" ~ comp;
+	string chd = format("x-ms-blob-type:%sBlob\nx-ms-date:%s\nx-ms-version:%s", toRFC822DateTimeString(time), StorageApiVersion, to!string(blobType));
 
 	string sigstr = format("%s\n%s\n%s\n%s\n%s\n%s",
 		to!string(req.method).toUpper(),
@@ -37,20 +38,21 @@ private string getGenericAuthorizationToken(AzureStorageClient conn, HTTPClientR
 	return urlEncode(format("SharedKeyLite %s:%s", conn.Account, Base64.encode(hmac.finish())));
 }
 
-public void writeGenericRequiredHeaders(AzureStorageClient conn, HTTPClientRequest req, string path, string comp = null)
+public void writeRequiredHeaders(AzureStorageBlobClient conn, HTTPClientRequest req, AzureBlobType blobType, string path, string comp = null)
 {
 	//Write required headers
 	SysTime reqTime = Clock.currTime().toUTC();
-	req.headers.addField("Authorization", getGenericAuthorizationToken(conn, req, path, comp, reqTime));
+	req.headers.addField("Authorization", getAuthorizationToken(conn, req, path, comp, reqTime));
 	req.headers.addField("x-ms-date", toRFC822DateTimeString(reqTime));
 	req.headers.addField("x-ms-version", StorageApiVersion);
+	req.headers.addField("x-ms-blob-type", to!string(blobType) ~ "Blob");
 }
 
 private string getTableAuthorizationToken(AzureStorageClient conn, string path, string comp, SysTime time)
 {
 	string crs = format("/%s/%s", conn.Account, path);
-	if(comp !is null || comp != "") crs ~= "?comp=" ~ comp;
-	string chd = format("x-ms-date:%s\nx-ms-version:%s\n", toRFC822DateTimeString(time), StorageApiVersion);
+	if(comp !is null && comp != "") crs ~= "?comp=" ~ comp;
+	string chd = format("x-ms-date:%s\nx-ms-version:%s", toRFC822DateTimeString(time), StorageApiVersion);
 
 	string sigstr = format("%s\n%s\n%s",
 		toRFC822DateTimeString(time),
